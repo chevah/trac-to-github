3aacb0a4d9aa99a517f5dcc41fe4e34275b0e50b
Berk Açıkel <berk@dopigo.com>
2020-12-30 13:19:36 +0300

    handle http urls

--- wiki-trac-rst-convert.py
+++ wiki-trac-rst-convert.py
@@ -10,8 +10,6 @@ def main():
     """
     Do the job.
     """
-
-
     if len(sys.argv) != 2:
         print("Need to pass the path to wiki base directory.")
         sys.exit(1)
@@ -19,81 +17,6 @@ def main():
     for root, _, files in os.walk(sys.argv[1]):
         for name in files:
             convert_file(os.path.join(root, name))
-
-
-def convert_file(path):
-    """
-    """
-    print('Converting ', path)
-    if 'Administrative.rst' in path:    
-        file = open(path, "r")
-        text = file.readlines()
-        convert_content(text, path)
-
-
-
-def convert_content(text, path):
-    """
-    Convert from Trac wiki RST format to standard RST format.
-    """
-    #import pdb; import sys; sys.stdout = sys.__stdout__; pdb.set_trace()
-
-    title_index = '[[TitleIndex'
-
-    string1 = ''
-    for line in text:
-
-        # Remove RST wrapping.
-        result = line.strip('}}}')
-        result = result.strip('{{{')
-        result = result.strip('#!rst')
-        result = result.strip()
-
-
-        result = result.replace(':trac:`', '')
-        #only for main pagess
-        if '[wiki:' not in result:
-            result = result.replace('wiki:', '(' + wiki_url)
-        
-        result = result.replace('`', ')')
-        result = result.replace('*', '')
-        result = result.strip()
-
-        if wiki_url in result:
-
-            a = result.split(wiki_url)
-            format_link = a[1].replace(')', '')
-            result = '* [' + format_link +']' + result
-        
-        
-        result += '\n'
-        if title_index in result:
-            result = write_sub_links(result, path)
-
-        string1 = string1 + result
-
-    return string1# Convert local files formated in Trac Wiki RST to Vanilla RST.
-import sys
-import os
-
-
-wiki_url = 'https://github.com/chevah/server/wiki/'
-
-
-def main():
-    """
-    Do the job.
-    """
-
-
-    if len(sys.argv) != 2:
-        print("Need to pass the path to wiki base directory.")
-        sys.exit(1)
-
-    for root, _, files in os.walk(sys.argv[1]):
-        for name in files:
-            convert_file(os.path.join(root, name))
-
 
 def convert_file(path):
     """
@@ -103,8 +26,6 @@ def convert_file(path):
         file = open(path, "r")
         text = file.readlines()
         convert_content(text, path)
-
-
 
 def convert_content(text, path):
     """
@@ -177,7 +98,6 @@ def convert_content(text, path):
 
     #return result
 
-
 def write_sub_links(text, path):
    # import pdb; import sys; sys.stdout = sys.__stdout__; pdb.set_trace()
 
@@ -199,7 +119,6 @@ def write_sub_links(text, path):
 
 
     return sub_links
-
 
 def format_name(text):
     """
