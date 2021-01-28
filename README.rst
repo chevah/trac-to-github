f1c19a7b048cb5d3c723cfc003deb70d2d1edbb3
Dan Haiduc <danuthaiduc@gmail.com>
2021-01-28 13:12:00 +0200

    Process TracWiki links and formatting. (#4)
    * WIP ticket #4: TracWiki-format wiki and ticket links
    
    * Process TracWiki format: links, headings, and lists
    
    * Update readme with todo or manual edits.
    
    * Add example for nested sidebar.
    
    * Fix list formating.
    
    * Add info about GitHub meta pages.
    
    Co-authored-by: Adi Roiban <adi.roiban@chevah.com>

--- README.rst
+++ README.rst
@@ -14,17 +14,31 @@ Create a virtualenv::
     mv config.py.sample config.py
 
 
-For wiki migration::
+For wiki migration.
+All pages are generated into a flat file structure.
+Spaces are used instead of path separators::
 
     python wiki_migrate.py PATH/TO/Trac.DB PATH/TO/GIT-REPO
 
-You might want to add a `_Sidebar.rst` file in the root wit::
+You might want to add a `_Sidebar.rst` file in the root with::
 
     * `<Administrative>`_
     * `<Development>`_
     * `<Infrastructure>`_
+
+      * `Services <Infrastructure-Services>`_
+      * `Machines <Infrastructure-Machines>`_
+
     * `<Support>`_
 
 For wiki content conversion::
 
     python wiki_trac_rst_convert.py PATH/TO/GIT-REPO
+
+
+Things that are not yet auto-converted:
+
+* TracWiki 3rd level heading `=== Some sub-section ===`
+* Sub-pages listing macro `[[TitleIndex(Development/)]]`
+* Local table of content `[[PageOutline]]`
+* Manually create _Sidebar.rst and _Footer.rst GitHub wiki meta-pages.
--- wiki_trac_rst_convert.py
+++ wiki_trac_rst_convert.py
@@ -3,6 +3,8 @@ import re
 import sys
 import os
 
+from config import TRAC_TICKET_PREFIX
+
 
 def main():
     """
@@ -38,42 +40,211 @@ def convert_content(text: str):
 
     * Remove RST wrapping
     * Convert Trac wiki directives to GitHub wiki links.
+    * Convert TracWiki headings, subheadings, and lists to RST.
     """
 
     to_remove = ['{{{', '#!rst', '}}}']
     for seq in to_remove:
         text = text.replace(seq, '')
     text = text.strip() + '\n'
-    text = _trac_rst_wiki_to_github_links(text)
+    text = _trac_to_github_wiki_links(text)
+    text = _tracwiki_to_rst_links(text)
+    text = _tracwiki_wiki_link_with_text_to_github_links(text)
+    text = _trac_ticket_links(text)
+    text = _tracwiki_heading_to_rst_heading(text)
+    text = _tracwiki_subheading_to_rst_subheading(text)
+    text = _tracwiki_list_dedent(text)
+    text = _tracwiki_list_separate_from_paragraph(text)
 
     return text
 
 
-def _trac_rst_wiki_to_github_links(text: str):
+def _trac_to_github_wiki_links(text: str):
     """
-    Takes RST content with Trac wiki link directives
-    and coverts the directives to inline GitHub wiki links.
+    Takes content with Trac wiki link directives and coverts
+    the directives to inline GitHub wiki links.
     """
 
-    link_matchers =[re.compile(r) for r in [
+    link_matchers = [
+        # RST markup:
         ':trac:`wiki:(.+?)`',
-        '`wiki:(.+?)`:trac:'
-    ]]
+        '`wiki:(.+?)`:trac:',
+
+        # TracWiki markup:
+        '`\[wiki:"?([^ ]+?)"?]`:trac:',
+        '\[wiki:"?([^ ]+?)"?]',
+    ]
 
     for link_re in link_matchers:
-        wiki_titles = re.findall(link_re, text)
-        for title in wiki_titles:
-            text = re.sub(
-                link_re,
-                rf'`<{_wiki_url(title)}>`_',
-                text,
-                1
-            )
+        for title in _matches(link_re, text):
+            text = _sub(link_re, f'`<{_wiki_url(title)}>`_', text)
 
     return text
 
 
-def _wiki_url(title):
+def _tracwiki_to_rst_links(text: str):
+    """
+    Takes TracWiki markup and converts its links to RST links.
+    """
+
+    url = '[a-z]+://[^ ]+'
+    link_text = '[^]]+'
+    link_re = f'\[({url}) ({link_text})]'
+
+    for url, link_text in _matches(link_re, text):
+        text = _sub(link_re, f'`{link_text} <{url}>`_', text)
+
+    return text
+
+
+def _tracwiki_wiki_link_with_text_to_github_links(text: str):
+    """
+    Takes TracWiki markup and converts its Wiki links which have
+    explicit link text into RST links.
+    If the link text is the same as the article name, generate a more
+    compact syntax.
+    """
+
+    title = '[^ ]+'
+    link_text = '[^]]+'
+
+    link_matchers = [
+        f'`\[wiki:({title}) ({link_text})]`:trac:',
+        f'\[wiki:({title}) ({link_text})]',
+    ]
+
+    for link_re in link_matchers:
+        for title, link_text in _matches(link_re, text):
+            if title == link_text:
+                text = _sub(link_re, f'`<{_wiki_url(title)}>`_', text)
+            else:
+                replacement = f'`{link_text} <{_wiki_url(title)}>`_'
+                text = _sub(link_re, replacement, text)
+
+    return text
+
+
+def _trac_ticket_links(text: str):
+    """
+    Replace Trac reference to ticket with an RST link to the ticket.
+    """
+
+    ticket_re = ':trac:`#([0-9]+)`'
+    for ticket in _matches(ticket_re, text):
+        text = _sub(
+            ticket_re,
+            f'`Trac #{ticket} <{TRAC_TICKET_PREFIX}{ticket}>`_',
+            text
+        )
+    return text
+
+
+def _tracwiki_heading_to_rst_heading(text: str):
+    """
+    Convert TracWiki 1st level headings to RST heading.
+
+    TracWiki:
+
+        = Some Top Heading =
+        Content here
+
+    RST conversion:
+
+        Some Top Heading
+        ================
+
+        Content here
+    """
+    heading_re = '^= (.*) =$'
+    for match in _matches(heading_re, text):
+        text = _sub(heading_re, _underline(match, '='), text)
+
+    return text
+
+
+def _tracwiki_subheading_to_rst_subheading(text: str):
+    """
+    Convert TracWiki 2nd level headings to RST heading.
+
+    TracWiki:
+
+        == Some 2nd Heading ==
+        Content here
+
+    RST conversion:
+
+        Some 2nd Heading
+        ----------------
+        Content here
+    """
+    heading_re = '^== (.*) ==$'
+    for match in _matches(heading_re, text):
+        text = _sub(heading_re, _underline(match, '-'), text)
+
+    return text
+
+
+def _tracwiki_list_dedent(text: str):
+    """
+    Remove a space before a list item, if exactly one space is
+    before the asterisk.
+    """
+
+    indented_list_item_re = '^ \* '
+    for _ in _matches(indented_list_item_re, text):
+        text = _sub(indented_list_item_re, '* ', text)
+
+    return text
+
+
+def _tracwiki_list_separate_from_paragraph(text: str):
+    """
+    During conversion from TracWiki to RST, ensure an empty line
+    between each non-list-item and the list item following it, if any.
+    """
+
+    lines = text.split('\n')
+    newlines = []
+    was_list_item_or_blank = True
+
+    for l in lines:
+        is_list_item = re.match('^ *\* .*', l)
+        if is_list_item:
+            if not was_list_item_or_blank:
+                newlines.append('')
+            was_list_item_or_blank = True
+        else:
+            is_empty = l.strip() == ''
+            was_list_item_or_blank = is_empty
+        newlines.append(l)
+
+    return '\n'.join(newlines)
+
+
+def _underline(text: str, line_symbol: str):
+    """
+    Add a line made of `line_symbol` after given `text`,
+    and return new text.
+    """
+    return text + "\n" + line_symbol * len(text)
+
+
+def _matches(pattern: str, text: str):
+    """
+    Return all matches of a particular `pattern` occurring in `text`.
+    """
+    return re.findall(pattern, text, flags=re.MULTILINE)
+
+
+def _sub(regex: str, replacement: str, text: str):
+    """
+    Substitute one occurrence of `regex` in `text` with `replacement`.
+    Return the resulting new text.
+    """
+    return re.sub(regex, replacement, text, count=1, flags=re.MULTILINE)
+
+
+def _wiki_url(title: str):
     """
     GitHub Wiki collapses directory structure.
 
